6d6ef5ba79b38f2005079589d46b872576497793
unknown <greentreerecord@gmail.com>
2026-04-11 05:33:38 -0700

    fix prisma line

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"]
+    serverComponentsExternalPackages: ["@prisma/client"]
   }
 }
+
+export default nextConfig  
