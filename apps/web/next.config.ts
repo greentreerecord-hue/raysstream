e4fd76dc6e9bb3be5d3edcb5fb72e4d34bc88aba
unknown <greentreerecord@gmail.com>
2026-04-10 20:56:36 -0700

    final prisma fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"]
   }
 };
 
-export default nextConfig;
+export default nextConfig;;
