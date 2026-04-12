a3bcd22d2c35903e368b3cb7cd4b8229f2717e66
unknown <greentreerecord@gmail.com>
2026-04-11 18:42:27 -0700

    fix app folder

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Ray'sStream 🚀</h1>
-      <p>Your app is live!</p>
-    </main>
-  );
+  return <h1>Ray'sStream 🚀</h1>;
 } 
