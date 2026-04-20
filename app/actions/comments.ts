4080e85406d0be402aaecfbbf0200ce5e472e5dd
unknown <greentreerecord@gmail.com>
2026-04-20 11:23:08 -0700

    disable comments temporarily

--- app/actions/comments.ts
+++ app/actions/comments.ts
@@ -1,28 +1,5 @@
 "use server";
 
-import { prisma } from "@/lib/prisma";
-
-export async function createComment({
-  videoId,
-  userId,
-  text,
-}: {
-  videoId: string;
-  userId: string;
-  text: string;
-}) {
-  try {
-    const comment = await prisma.comment.create({
-      data: {
-        videoId,
-        authorId: userId,
-        text,
-      },
-    });
-
-    return { success: true, comment };
-  } catch (error) {
-    console.error(error);
-    return { success: false };
-  }
+export async function createComment() {
+  return { success: true };
 } 
