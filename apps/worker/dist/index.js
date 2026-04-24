6f2794a1a338c4d22569ba750acf146b8d176d26
unknown <greentreerecord@gmail.com>
2026-04-24 11:32:32 -0700

    cleanup: ignore build cache files

--- apps/worker/dist/index.js
+++
@@ -1,8 +0,0 @@
-import { Worker } from "bullmq";
-import { transcodeVideo } from "./jobs/transcode";
-const connection = { url: process.env.REDIS_URL };
-new Worker("media", async (job) => {
-    if (job.name === "transcode-video") {
-        await transcodeVideo(job.data);
-    }
-}, { connection });
