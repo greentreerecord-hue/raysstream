1f6485742595992208e8cb74615821cbf7608b2c
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-18 12:34:30 -0700

    Delete lib directory

--- lib/auth.ts
+++
@@ -1,9 +0,0 @@
-export async function login(email: string, password: string) {
-  console.log("Login attempt:", { email, password });
-
-  // TEMP: fake user (until real DB is added)
-  return {
-    id: "1",
-    email,
-  };
-} 
